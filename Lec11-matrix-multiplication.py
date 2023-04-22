7d436a1f1ef944a7abfd762e63f22547288078b0
n132 <xm2146@nyu.edu>
2023-04-22 16:33:58 -0400

    Update Lec11-matrix-multiplication.py

---
+++ Lec11-matrix-multiplication.py
@@ -0,0 +1,13 @@
+import math
+input_matrix  =  [30,1,40,10,25]
+def matrix_chain_multiplication(A):
+    n  = len(A)-1
+    T = [[0 for __ in range(n+1)] for _ in range(n+1)]
+    for off in range(1,n):
+        for start in range(n,0,-1):
+            end = start + off
+            if end>n:
+                continue
+            T[start][end] = min([T[start][t]+T[t+1][end]+A[start-1]*A[t]*A[end] for t in range(start,end)])
+    return T[1][n]
+print(matrix_chain_multiplication(input_matrix))
