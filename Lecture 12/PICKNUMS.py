a135fc8d9a36c1f1983fcac5fab08d5f7faaf338
n132 <xm2146@nyu.edu>
2023-05-14 17:18:49 -0400

    Update PICKNUMS.py

--- Lecture 12/PICKNUMS.py
+++ Lecture 12/PICKNUMS.py
@@ -2,4 +2,4 @@ def pickNumbers(A):
     if len(A)<=2:
         return min(A)
     return min([A[0]+sum(A[1:])-pickNumbers(A[1:]),A[-1]+sum(A[:-1])-pickNumbers(A[:-1])])
-print(pickNumbers([100,3,100,3]))
+print(pickNumbers([8, 15, 3, 7]))
